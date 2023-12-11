2dd6a9d499e40c95440794a6196063725d2cba09
sgdavies <samgdavies@gmail.com>
2023-12-11 18:16:17 +0000

    Boat races - didn't need big ints after all

--- 06.go
+++ 06.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"math"
-	"math/big"
 )
 
 var TEST = false
@@ -44,24 +43,9 @@ func day06() {
 	// x = (-b +/- sqrt(b^2 - 4ac) / 2a) ; a=-1, b=t, c=-D
 	// ==0 when p = (-t +/- sqrt(t^2 -4.D) ) / -2
 	// ==0 when p = 0.5*(t +/- sqrt(t^2 -4.D))
-
-	// Easy solution - normal ints (not good enough for real data)
-	// lower := math.Ceil(0.5 * (float64(time) - math.Sqrt(float64(time*time-4*distance))))
-	// upper := math.Floor(0.5 * (float64(time) + math.Sqrt(float64(time*time-4*distance))))
-	// fmt.Println(upper - lower + 1)
-
-	// Use big ints for real data
-	lowerf := big.NewFloat(0.5 * (float64(time) - math.Sqrt(float64(time*time-4*distance))))
-	upperf := big.NewFloat(0.5 * (float64(time) + math.Sqrt(float64(time*time-4*distance))))
-	lower, _ := lowerf.Int(nil)
-	if !lowerf.IsInt() {
-		lower.Add(lower, big.NewInt(1)) // Int() rounds down - we want to round up to the first win
-	}
-	upper, _ := upperf.Int(nil) // Round down to get the last win
-	// fmt.Println(lowerf, lower, upperf, upper)
-	wins := upper.Sub(upper, lower)
-
-	fmt.Println(wins.Add(wins, big.NewInt(1))) // +1 - count both ends of range
+	lower := math.Ceil(0.5 * (float64(time) - math.Sqrt(float64(time*time-4*distance))))
+	upper := math.Floor(0.5 * (float64(time) + math.Sqrt(float64(time*time-4*distance))))
+	fmt.Println(int(upper - lower + 1))
 }
 
 func winRace(press, time, dist int) bool {
